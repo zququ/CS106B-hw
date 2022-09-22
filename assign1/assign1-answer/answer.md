95f51e029cb694beb51b4c11d2e7d50df3cc6509
“zququ” <qzh813@gmail.com>
2022-09-22 15:40:27 +0800

    push

--- assign1/assign1-answer/answer.md
+++ assign1/assign1-answer/answer.md
@@ -10,12 +10,12 @@
 
 `findPerfects` 执行时间为：
 
-| number | time/s |
-|--------|--------|
-| 15000  | 0.931  |
-| 30000  | 3.650  |
-| 60000  | 14.282 |
-| 120000 | 57.243 |
+| number | time (s) |
+|--------|----------|
+| 15000  | 0.931    |
+| 30000  | 3.650    |
+| 60000  | 14.282   |
+| 120000 | 57.243   |
 
 作图如下：
 
@@ -29,14 +29,14 @@
 
 ### Q4
 
-由 Q2 可见时间约为 4396972 秒。
+由 Q2 计算 $f(33550336)$ 时间约为 4396972 秒。
 
 ### 深入测试
 
 `total` 的初始值从 0 改为 1，测试错误如下：
 
 ```
-Incorrect (PROVIDED_TEST, perfect.cpp:94) Confirm divisorSum of small inputs
+Incorrect (PROVIDED\_TEST, perfect.cpp:94) Confirm divisorSum of small inputs
 Line 95: EXPECT_EQUAL failed: divisorSum(1) != 0
 divisorSum(1) = 1
 ```
@@ -45,7 +45,71 @@ divisorSum(1) = 1
 
 其他测试不能通过，因为改变 `total` 的初始值会造成 `divisorSum()` 的计算错误。
 
+### Q6
 
+测试策略包括以下几个方面：
+
+1. 负数测试
+2. 完全数的测试
+3. 非完全数的测试
+4. 边缘测试，平方数的测试
+5. 速度测试
+
+### Q7
+
+测试结果见下图：
+
+![p3](assets/smarter-student-test.png) 
+
+时间测试结果如下表格：
+
+| number  | time (s) |
+|---------|----------|
+| 500000  | 2.667    |
+| 1000000 | 7.314    |
+| 2000000 | 20.635   |
+| 4000000 | 57.368   |
+
+### Q8
+
+将 Q7 中数据做曲线的手动估计拟合，得到函数关系如下图：
+
+![p4](assets/smarter-predicted-time-for-5th.png) 
+
+可以计算出 `findPerfectsSmarter` 找到第五个完全数需要大约 1385 s。
+
+
+### Q9
+
+```cpp
+// student test for findNthPerfectEuclid()
+STUDENT_TEST("Confirm find -1 perfect number is ok."){
+    EXPECT(!isPerfect(findNthPerfectEuclid(-1)));
+}
+
+// edged test
+
+STUDENT_TEST("Confirm find 0 perfect number is will not make troubles."){
+    EXPECT(!isPerfect(findNthPerfectEuclid(0)));
+}
+
+STUDENT_TEST("Confirm find 5 perfect number is ok." ){
+    EXPECT(isPerfect(findNthPerfectEuclid(5)));
+}
+
+// large number calculation test
+STUDENT_TEST("Confirm find 6 perfect number is ok." ){
+    EXPECT(isPerfect(findNthPerfectEuclid(6)));
+}
+```
+
+`findNthPerfectEuclid(-1)`  --> 负值检测，虽然一般不会用到，防止出现奇怪问题；
+
+`findNthPerfectEuclid(0)`  --> 同上；
+
+`findNthPerfectEuclid(5)` --> 正常检测；
+
+`findNthPerfectEuclid(6)` --> 机器能力内，较大计算量的压力测试。
 
 
 
--- assign1/assign1-starter/perfect.cpp
+++ assign1/assign1-starter/perfect.cpp
@@ -59,7 +59,6 @@ void findPerfects(long stop) {
  */
 
 long smarterSum(long n) {
-    /* TODO: Fill in this function. */
     long total = 0;
     for (long divisor = 2; divisor <= sqrt(n); divisor++){
         if (n % divisor == 0){
@@ -69,15 +68,23 @@ long smarterSum(long n) {
     return total+1;
 }
 
-/* TODO: Replace this comment with a descriptive function
- * header comment.
+/* SmarterVersion
+ * --------------
+ * This function takes one argument `n` and returns a boolean
+ * (true/false) value indicating whether or not `n` is perfect.
+ * A perfect number is a non-zero positive number whose sum
+ * of its proper divisors is equal to itself.
  */
 bool isPerfectSmarter(long n) {
     return (n != 0) && (n != 1) && (n == smarterSum(n));
 }
 
-/* TODO: Replace this comment with a descriptive function
- * header comment.
+/* SmarterVersion
+ * --------------
+ * This function does an exhaustive search for perfect numbers.
+ * It takes one argument `stop` and searches the range 1 to `stop`,
+ * checking each number to see whether it is perfect and if so,
+ * printing it to the console.
  */
 void findPerfectsSmarter(long stop) {
     for (long num = 1; num < stop; num++) {
@@ -93,10 +100,27 @@ void findPerfectsSmarter(long stop) {
  * header comment.
  */
 long findNthPerfectEuclid(long n) {
-    /* TODO: Fill in this function. */
-    return 0;
+    if (n == 0){
+        return NULL;
+    }
+    if (n < 0){
+        return NULL;
+    }
+    long count = 0;
+    long MaxPerfect_m = 0;
+    for (long k = 1; n > 0; k++){
+        long m = pow(2, k) - 1;
+        if (smarterSum(m) == 1 && m!=1){
+            long Perfect_m = pow(2, k-1) * (pow(2, k) - 1);
+            cout << "Found perfect number: " << Perfect_m << endl;
+            n--;
+            count ++;
+            MaxPerfect_m = Perfect_m;
+        }
+    }
+//    cout << endl << "Done searching up to " << count << endl;
+    return MaxPerfect_m;
 }
-
 
 /* * * * * * Test Cases * * * * * */
 
@@ -105,60 +129,97 @@ long findNthPerfectEuclid(long n) {
  * provided tests.
  */
 
-PROVIDED_TEST("Confirm divisorSum of small inputs") {
-    EXPECT_EQUAL(divisorSum(1), 0);
-    EXPECT_EQUAL(divisorSum(6), 6);
-    EXPECT_EQUAL(divisorSum(12), 16);
+//PROVIDED_TEST("Confirm divisorSum of small inputs") {
+//    EXPECT_EQUAL(divisorSum(1), 0);
+//    EXPECT_EQUAL(divisorSum(6), 6);
+//    EXPECT_EQUAL(divisorSum(12), 16);
+//}
+
+//PROVIDED_TEST("Confirm 6 and 28 are perfect") {
+//    EXPECT(isPerfect(6));
+//    EXPECT(isPerfect(28));
+//}
+
+//PROVIDED_TEST("Confirm 12 and 98765 are not perfect") {
+//    EXPECT(!isPerfect(12));
+//    EXPECT(!isPerfect(98765));
+//}
+
+//PROVIDED_TEST("Test oddballs: 0 and 1 are not perfect") {
+//    EXPECT(!isPerfect(0));
+//    EXPECT(!isPerfect(1));
+//}
+
+//PROVIDED_TEST("Confirm 33550336 is perfect") {
+//    EXPECT(isPerfect(33550336));
+//}
+
+//PROVIDED_TEST("Time trials of findPerfects on doubling input sizes") {
+//    TIME_OPERATION(15000, findPerfects(15000));
+//    TIME_OPERATION(30000, findPerfects(30000));
+//    TIME_OPERATION(60000, findPerfects(60000));
+//    TIME_OPERATION(120000, findPerfects(120000));
+//}
+
+//// TODO: add your student test cases here
+
+//// negative value test
+//STUDENT_TEST("Confirm -6 and -28 are not perfect") {
+//    EXPECT(!isPerfect(-6));
+//    EXPECT(!isPerfect(-28));
+//}
+//// perfect number test
+//STUDENT_TEST("Confirm 6 and 28 are perfect") {
+//    EXPECT(isPerfectSmarter(6));
+//    EXPECT(isPerfectSmarter(28));
+//}
+
+//// bigger perfect number test
+//STUDENT_TEST("Confirm 33550336 is perfect") {
+//    EXPECT(isPerfectSmarter(33550336));
+//}
+
+//// edged number test
+//STUDENT_TEST("Confirm 25 is not perfect") {
+//    EXPECT(!isPerfectSmarter(25));
+//}
+
+//speed test
+//STUDENT_TEST("Time trials of findPerfects on doubling input sizes") {
+//    TIME_OPERATION(500000, findPerfectsSmarter(500000));
+//    TIME_OPERATION(1000000, findPerfectsSmarter(1000000));
+//    TIME_OPERATION(2000000, findPerfectsSmarter(2000000));
+//    TIME_OPERATION(4000000, findPerfectsSmarter(4000000));
+//}
+
+
+// student test for findNthPerfectEuclid()
+
+//negative test
+STUDENT_TEST("Confirm find 1 perfect number is ok."){
+    EXPECT(isPerfect(findNthPerfectEuclid(1)));
 }
 
-PROVIDED_TEST("Confirm 6 and 28 are perfect") {
-    EXPECT(isPerfect(6));
-    EXPECT(isPerfect(28));
+// edged test
+
+STUDENT_TEST("Confirm find 0 perfect number is will not make troubles."){
+    EXPECT(!isPerfect(findNthPerfectEuclid(0)));
 }
 
-PROVIDED_TEST("Confirm 12 and 98765 are not perfect") {
-    EXPECT(!isPerfect(12));
-    EXPECT(!isPerfect(98765));
+STUDENT_TEST("Confirm find 5 perfect number is ok." ){
+    EXPECT(isPerfect(findNthPerfectEuclid(5)));
 }
 
-PROVIDED_TEST("Test oddballs: 0 and 1 are not perfect") {
-    EXPECT(!isPerfect(0));
-    EXPECT(!isPerfect(1));
+// large number calculation test
+STUDENT_TEST("Confirm find 6 perfect number is ok." ){
+    EXPECT(isPerfect(findNthPerfectEuclid(6)));
 }
 
-PROVIDED_TEST("Confirm 33550336 is perfect") {
-    EXPECT(isPerfect(33550336));
-}
-
-PROVIDED_TEST("Time trials of findPerfects on doubling input sizes") {
-    TIME_OPERATION(15000, findPerfects(15000));
-    TIME_OPERATION(30000, findPerfects(30000));
-    TIME_OPERATION(60000, findPerfects(60000));
-    TIME_OPERATION(120000, findPerfects(120000));
-}
-
+// speed test for findNthPerfectEuclid(5)
 STUDENT_TEST("Time trials of findPerfects on doubling input sizes") {
-    TIME_OPERATION(15000, findPerfectsSmarter(15000));
-    TIME_OPERATION(30000, findPerfectsSmarter(30000));
-    TIME_OPERATION(60000, findPerfectsSmarter(60000));
-    TIME_OPERATION(120000, findPerfectsSmarter(120000));
-}
-
-// TODO: add your student test cases here
-STUDENT_TEST("Confirm -6 and -28 are perfect") {
-    EXPECT(isPerfect(-6));
-    EXPECT(isPerfect(-28));
-}
-
-STUDENT_TEST("Confirm 33550336 is perfect") {
-    EXPECT(isPerfect(33550336));
-}
-
-STUDENT_TEST("Confirm 6 and 28 are perfect") {
-    EXPECT(isPerfectSmarter(6));
-    EXPECT(isPerfectSmarter(28));
-}
-
-STUDENT_TEST("Confirm 33550336 is perfect") {
-    EXPECT(isPerfectSmarter(33550336));
+    TIME_OPERATION(1, findNthPerfectEuclid(1));
+    TIME_OPERATION(2, findNthPerfectEuclid(2));
+    TIME_OPERATION(3, findNthPerfectEuclid(3));
+    TIME_OPERATION(4, findNthPerfectEuclid(4));
+    TIME_OPERATION(5, findNthPerfectEuclid(5));
 }
