cdac170df658a77a20761137af2e3cf23dc4e597
“zququ” <qzh813@gmail.com>
2022-10-13 18:45:45 +0800

    add class

--- assign3.1/assign3.1-starter/balanced.cpp
+++ assign3.1/assign3.1-starter/balanced.cpp
@@ -14,10 +14,11 @@ using namespace std;
 /*
  * @brief   operatorsFrom
  * ----------------------
- * @detail  recursively process string and return only char
+ * @detail  recursively process string and return only symbol
  * @params  <string> str
  * @return  string
  */
+
 string operatorsFrom(string str) {
     // process the first char, `if` can be returned
     if (str.length()!=0){
@@ -27,14 +28,60 @@ string operatorsFrom(string str) {
 }
     return "";
 }
+
 /*
- * TODO: Replace this comment with a descriptive function
- * header comment.
+ * @brief   operatorsAreMatched
+ * ----------------------------
+ * @detail  check if operators are matched
+ *          balance only when:
+ *          1. string is blank
+ *          2. string includes "()", "[]", "{}" etc.
+ * @params  <string> ops
+ * @return  <bool>
  */
+
+#define v1
+#ifdef v1
 bool operatorsAreMatched(string ops) {
-    /* TODO: Implement this function. */
+    if (ops.length() != 0){
+        if (ops.find("()") != string::npos){
+            ops.erase(ops.find("()"), 2);
+            return operatorsAreMatched(ops);
+        }
+
+        if (ops.find("[]") != string::npos){
+            ops.erase(ops.find("[]"), 2);
+            return operatorsAreMatched(ops);
+        }
+
+        if (ops.find("{}") != string::npos){
+            ops.erase(ops.find("{}"), 2);
+            return operatorsAreMatched(ops);
+        }
+    }
+    else {
+        return true;
+    }
     return false;
 }
+#endif
+
+#ifdef v2
+bool operatorsAreMatched(string ops) {
+    if (!ops.empty()){
+    if (ops[0] + 1 == ops[1]){
+        ops.erase(0,2);
+        return operatorsAreMatched(ops);
+    }
+    else {
+        ops.replace(0, ops.length()-1);
+        return operatorsAreMatched(ops.substr(1));
+    }
+
+    return true;
+}
+#endif
+
 
 /*
  * The isBalanced function assumes correct implementation of
@@ -61,6 +108,9 @@ PROVIDED_TEST("operatorsFrom on simple example") {
 
 PROVIDED_TEST("operatorsAreMatched on simple example") {
     EXPECT(operatorsAreMatched("{}"));
+    EXPECT(operatorsAreMatched("(())"));
+    EXPECT(operatorsAreMatched("()[]"));
+    EXPECT(!operatorsAreMatched("("));
 }
 
 PROVIDED_TEST("isBalanced on example from writeup") {
@@ -74,7 +124,12 @@ PROVIDED_TEST("isBalanced on non-balanced examples from writeup") {
     EXPECT(!isBalanced("{ ( x } y )"));
 }
 
+/* * * * * * Student Cases * * * * * */
+
 STUDENT_TEST("mytest1")
 {
     EXPECT_EQUAL(operatorsFrom("ccaqwre()JLKJ{KJq123}"), "(){}");
+    EXPECT_EQUAL(operatorsFrom(""), "");
+    EXPECT_EQUAL(operatorsFrom("()"), "()");
 }
+
